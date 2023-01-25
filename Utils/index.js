ead2eb1b41ee0aab6e20deb91a760071bd5b5073
Poptartkay <fordjulietkay@gmail.com>
2023-01-25 14:01:58 -0800

    added bootstrap/ JS const/

---
+++ Utils/index.js
@@ -0,0 +1,11 @@
+const Employee = require("");
+const Manager = require("");
+const Intern = require("");
+const Engineer = require("");
+
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const jest = require("jest");
+
